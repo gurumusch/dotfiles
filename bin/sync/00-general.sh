f477f53b559e4971151acdf61dd2b2b7fe934ba9
Andreas Schäfer <9209378+gurumusch@users.noreply.github.com>
2023-03-16 11:41:22 +0100

    start theming wofi

--- bin/sync/00-general.sh
+++ bin/sync/00-general.sh
@@ -4,6 +4,39 @@
 echo "Ensure basic software is installed"
 sudo pacman -S git base-devel cmatrix terminator --needed --noconfirm 2>&1 | sed "s/^/   /"
 
+# Link bin folder in home
+config_in_home=~/bin
+config=`pwd`/bin/home
+echo "Checking bin folder (${config_in_home}) ..."
+if [ ! -e $config_in_home ]; then
+    ln -s $config $config_in_home 2>&1 | sed "s/^/   /"
+    echo "created link: ${config_in_home} -> ${config}"
+else
+    echo "exists"
+fi
+
+# Link profile in home
+config_in_home=~/.profile
+config=`pwd`/config/dotfiles/profile
+echo "Checking .profile (${config_in_home}) ..."
+if [ ! -e $config_in_home ]; then
+    ln -s $config $config_in_home 2>&1 | sed "s/^/   /"
+    echo "created link: ${config_in_home} -> ${config}"
+else
+    echo "exists"
+fi
+
+# Link Xresources in home
+config_in_home=~/.Xresources
+config=`pwd`/config/dotfiles/Xresources
+echo "Checking .Xresources (${config_in_home}) ..."
+if [ ! -e $config_in_home ]; then
+    ln -s $config $config_in_home 2>&1 | sed "s/^/   /"
+    echo "created link: ${config_in_home} -> ${config}"
+else
+    echo "exists"
+fi
+
 # Create the symbolic link for the terminator config
 tool_name="terminator"
 tool_config_folder="terminator"
