0e1a99db716114f0ae82e30fb6a1445242880fd8
Andreas Schäfer <9209378+gurumusch@users.noreply.github.com>
2023-03-23 19:44:22 +0100

    move more to eww

--- bin/home/wallpaper.sh
+++ bin/home/wallpaper.sh
@@ -6,16 +6,18 @@ files=(~/.config/hypr/wallpapers/*.jpg)
 hypr=~/.config/hypr
 scripts=$hypr/scripts
 
-index=$(cat $TEMP)
-index=$((index+1))
+if [ -f $TEMP ]; then
+   index=$(cat $TEMP)
+   index=$((index+1))
+else
+   index=0
+fi
+
 if [ $index -ge ${#files[@]} ]; then
     index=0
 fi
+
 echo $index > $TEMP
-#swww img "${files[$index]}"\
-#   --transition-type grow\
-#   --transition-pos "$(hyprctl cursorpos)"\
-#   --transition-duration 3
 swww img "${files[$index]}"\
    --transition-type center\
    --transition-duration 3
