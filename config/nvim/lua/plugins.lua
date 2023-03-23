b342c9422dfe7cf539a82d58973592221a06c180
Andreas Schäfer <9209378+gurumusch@users.noreply.github.com>
2023-03-23 22:56:26 +0000

    autoinstall vim packages

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,9 +1,23 @@
 -- Automatically run: PackerCompile
-vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
-    pattern = "plugins.lua",
-    command = "source <afile> | PackerCompile",
-})
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--    group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
+--    pattern = "plugins.lua",
+--    command = "source <afile> | PackerCompile",
+-- })
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 
 return require("packer").startup(function(use)
     -- Packer
@@ -173,4 +187,10 @@ return require("packer").startup(function(use)
             })
         end,
     })
+
+   -- Automatically set up your configuration after cloning packer.nvim
+   -- Put this at the end after all plugins
+   if packer_bootstrap then
+      require('packer').sync()
+   end
 end)
