0b08acdb390140b66dad403fc8e6b37a2e936ddd
Andreas Schäfer <9209378+gurumusch@users.noreply.github.com>
2023-03-25 20:33:22 +0000

    alacritty base config

--- bin/sync/04-alacritty.sh
+++ bin/sync/04-alacritty.sh
@@ -9,7 +9,7 @@ echo "Ensure $tool_name is installed"
 sudo pacman -S $tool_name --needed --noconfirm 2>&1 | sed "s/^/   /"
 
 # Link alacritty config in home
-config_in_home=~/config/$tool_name
+config_in_home=~/.config/$tool_name
 config=`pwd`/config/$tool_name
 echo "Checking $tool_name (${config_in_home}) ..."
 if [ ! -e $config_in_home ]; then
